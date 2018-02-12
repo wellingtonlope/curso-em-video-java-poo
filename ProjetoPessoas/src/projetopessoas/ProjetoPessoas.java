187d9624af1e685b2fda004961775eca7665a1b4
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-12 17:52:54 -0400

    Projeto pessoas

---
+++ ProjetoPessoas/src/projetopessoas/ProjetoPessoas.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopessoas;
+
+/**
+ *
+ * @author wellington
+ */
+public class ProjetoPessoas {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		Pessoa p1 = new Pessoa();
+		Aluno p2 = new Aluno();
+		Professor p3 = new Professor();
+		Funcionario p4 = new Funcionario();
+		
+		p1.setNome("Pedro");
+		p2.setNome("Maria");
+		p3.setNome("Cláudio");
+		p4.setNome("Fabiana");
+		
+		p2.setCurso("Informatica");
+		p3.setSalario(2500);
+		p4.setSetor("Estoque");
+		
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+		System.out.println(p4.toString());
+	}
+	
+}
